fb90083fb218097c57eb0dd98f6f540fad4f1c50
Holices Junior <dev.holices@gmail.com>
2025-02-19 09:48:53 +0000

    Add hover on button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,11 +1,11 @@
-import { ComponentProps, ReactNode } from "react"
+import { type ComponentProps, ReactNode } from 'react'
 
 interface ButtonProps extends ComponentProps<'button'> {}
 
 export function Button({ ...props }: ButtonProps) {
   return (
-    <button 
-      className="flex items-center justify-between w-full px-5 h-12 bg-gray-500 text-blue rounded-xl font-semibold cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300" 
+    <button
+      className="flex items-center justify-between w-full px-5 h-12 bg-gray-500 text-blue rounded-xl font-semibold cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
       {...props}
     />
   )
