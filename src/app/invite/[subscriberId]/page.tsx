4b7945959c69170e0d1305d073f0dc55e925393f
Holices Junior <dev.holices@gmail.com>
2025-02-28 17:55:31 +0000

    feat: moved files

---
+++ src/app/invite/[subscriberId]/page.tsx
@@ -0,0 +1,52 @@
+import Image from 'next/image'
+import logo from '../../../assets/logo.png'
+import InviteLinkInput from './invite-link-input'
+import { Ranking } from './ranking'
+import Stats from './stats'
+
+interface InvitePageProps {
+  params: Promise<{
+    subscriberId: string
+  }>
+}
+
+export default async function InvitePage(props: InvitePageProps) {
+  const { subscriberId } = await props.params
+
+  const inviteLink = `http://localhost:3333/invites/${subscriberId}`
+
+  return (
+    <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
+      <div className="flex flex-col gap-10 w-full max-w-[550px]">
+        <Image src={logo} alt="Rankify" width={108.5} height={30} />
+
+        <div className="space-y-2">
+          <h1 className="text-4xl font-semibold font-heading text-gray-100 leading-none">
+            Registration confirmed!
+          </h1>
+          <p className="text-gray-300">
+            To enter the event, access the link sent to your email.
+          </p>
+        </div>
+
+        <div className="space-y-6">
+          <div className="space-y-3">
+            <h2 className="text-gray-200 text-xl font-heading font-semibold leading-none">
+              Refer and earn
+            </h2>
+            <p className="text-gray-300">
+              Invite more people to the event and compete for exclusive
+              benefits! Just share the link below and follow the registrations:
+            </p>
+          </div>
+
+          <InviteLinkInput inviteLink={inviteLink} />
+
+          <Stats subscriberId={subscriberId} />
+        </div>
+      </div>
+
+      <Ranking />
+    </div>
+  )
+}
